b2f48d4d19ceba6a214c9b726222ad56c2c84502
Andy Loftus <aloftus@gmail.com>
2026-07-18 21:56:29 +0000

    mouse key overrides and layer2 shortcuts
    CTRL + SHIFT + 4 => Mouse Wheel Down
    CTRL + SHIFT + 2 => Mouse Wheel Up
    ALT + V => Mouse Button 3
    left pinky shift => ctrl-alt-shift (slack show unreads)
    left pinky ctlr => Caps Word Toggle
    F8 => Caps Word Toggle
    layer 2 + i => mouse wheel up
    layer 2 + k => mouse wheel down
    layer 2 + j => mouse wheel left
    layer 2 + l => mouse wheel right

--- keyboards/xbows/knight_plus/keymaps/andylytical/overrides.c
+++ keyboards/xbows/knight_plus/keymaps/andylytical/overrides.c
@@ -1,33 +1,58 @@
 /* See also: */
 /* https://docs.qmk.fm/features/key_overrides */
 /* Key Overrides */
+
 /* CTRL + PGUP => HOME */
-const key_override_t ctrl_pgup_home =
-  ko_make_basic(
-    MOD_MASK_CTRL,  // Trigger modifiers: any ctrl
-    KC_PGUP,        // Trigger key: page-up
-    KC_HOME         // Replacement: home
-  );
+/* const key_override_t ctrl_pgup_home = */
+/*   ko_make_basic( */
+/*     MOD_MASK_CTRL,  // Trigger modifiers: any ctrl */
+/*     KC_PGUP,        // Trigger key: page-up */
+/*     KC_HOME         // Replacement: home */
+/*   ); */
 
 /* CTRL + PGDOWN => END */
-const key_override_t ctrl_pgdown_end =
-  ko_make_basic(
-    MOD_MASK_CTRL,  // Trigger modifiers: any ctrl
-    KC_PGDN,        // Trigger key: page-down
-    KC_END          // Replacement: end
-  );
+/* const key_override_t ctrl_pgdown_end = */
+/*   ko_make_basic( */
+/*     MOD_MASK_CTRL,  // Trigger modifiers: any ctrl */
+/*     KC_PGDN,        // Trigger key: page-down */
+/*     KC_END          // Replacement: end */
+/*   ); */
 
 /* CTRL + UP => CAPSWORD */
-const key_override_t ctrl_up_capsword =
+/* const key_override_t ctrl_up_capsword = */
+/*   ko_make_basic( */
+/*     MOD_MASK_CTRL,      // Trigger modifiers: any ctrl */
+/*     KC_UP,              // Trigger key: page-down */
+/*     QK_CAPS_WORD_TOGGLE // Replacement: end */
+/*   ); */
+
+/* ALT + V => Mouse Button 3 */
+const key_override_t alt_v_mouse3 =
   ko_make_basic(
-    MOD_MASK_CTRL,      // Trigger modifiers: any ctrl
-    KC_UP,              // Trigger key: page-down
-    QK_CAPS_WORD_TOGGLE // Replacement: end
+    MOD_MASK_ALT,       // Trigger modifiers: any alt
+    KC_V,               // Trigger key: page-down
+    MS_BTN3             // Replacement: mouse button 3
+  );
+
+/* CTRL + SHIFT + 4 => Mouse Wheel Down */
+const key_override_t ctl_shft_4_mswheeldown =
+  ko_make_basic(
+    MOD_MASK_CS,        // Trigger modifiers: any ctrl + shft
+    KC_4,               // Trigger key: 4
+    MS_WHLD             // Replacement: mouse wheel down
+  );
+
+/* CTRL + SHIFT + 2 => Mouse Wheel Up */
+const key_override_t ctl_shft_2_mswheelup =
+  ko_make_basic(
+    MOD_MASK_CS,        // Trigger modifiers: any ctrl + shft
+    KC_2,               // Trigger key: 2
+    MS_WHLU             // Replacement: mouse wheel up
   );
 
 // This globally defines all key overrides to be used
 const key_override_t *key_overrides[] = {
-	&ctrl_pgup_home,
-	&ctrl_pgdown_end,
-  &ctrl_up_capsword
+	&alt_v_mouse3,
+	&ctl_shft_4_mswheeldown,
+  &ctl_shft_2_mswheelup
 };
